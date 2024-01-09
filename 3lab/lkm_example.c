b546857c4edce1217bfc9e3220662370b16a371f
artemskorypin <55595651+artemskorypin@users.noreply.github.com>
2024-01-09 19:01:39 +0700

    Update lkm_example.c

--- 3lab/lkm_example.c
+++ 3lab/lkm_example.c
@@ -8,13 +8,13 @@ MODULE_DESCRIPTION("A simple example Linux module.");
 
 
 static int __init lkm_example_init(void) {
- printk(KERN_INFO "Welcome to the Tomsk State University!\n");
+ pr_info("Welcome to the Tomsk State University!\n");
      /* Если вернётся не 0, значит, init_module провалилась; модули загрузить не получится. */
 
  return 0;
 }
 static void __exit lkm_example_exit(void) {
- printk(KERN_INFO "Tomsk State University forever!\n");
+ pr_info("Tomsk State University forever!\n");
 }
 module_init(lkm_example_init);
 module_exit(lkm_example_exit);
