4a2b2f43704b61cdcbfa5f08e80cb730b81f0220
artemskorypin <55595651+artemskorypin@users.noreply.github.com>
2024-01-28 17:50:45 +0700

    Add files via upload

--- 4lab/lkm_example.c
+++ 4lab/lkm_example.c
@@ -1,70 +1,64 @@
-#include <linux/module.h>//нужен для всех модулей
-#include <linux/fs.h>//для работы с файлами
-#include <linux/time.h>//для работы с временем
-#include <linux/delay.h>//для задержек
+#include <linux/module.h>/* Необходим всем модулям */
+#include <linux/kernel.h>/* Необходим для pr_info() */
+#include <linux/proc_fs.h>//для работы с файлами
+#include <linux/time.h>//для работы со временем
 
-#define FILENAME "example.txt"
+#define PROC_NAME "tsulab"
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Artem Skoriupin");
+MODULE_AUTHOR("Skoriupin Artem");
 MODULE_DESCRIPTION("A simple example Linux module for time.");
 
-static int file_read_count = 0;
+static int read_count = 0;
 
-static int __init my_init(void) {
-    struct file *file;
+static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos) {
 
-    while(file_read_count < 3)
-    {
-    	file = filp_open(FILENAME, O_RDONLY, 0);//открыли файл
-    	if (IS_ERR(file)) {//проверили, что открылся
-        	pr_info("Error opening file\n");
-        	return PTR_ERR(file);
-    	}
-
-    	long day = (24 * 60 * 60);   
-    	struct timespec64 current_time;
-    	ktime_get_real_ts64(&current_time);//текущее время с начала эпохи
+    struct timespec64 current_time;
+    ktime_get_real_ts64(&current_time);
     
-    	long long times = current_time.tv_sec;
-	    
-	// Получаем количество секунд с начала дня
-    	long seconds_since_midnight = (times) % day;
+    // Получаем количество секунд с начала часа
+    long long times = current_time.tv_sec;
+
+    long seconds_since_midnight = (times) % (60*60);
 	
-    	// Вычисляем количество минут
-    	int minutes_since_midnight = seconds_since_midnight / 60;
+    // Вычисляем количество минут
+    int minutes_since_midnight = seconds_since_midnight / 60;
 
-    	int elapsed_minutes = minutes_since_midnight + 11*60;
-    	int remaining_minutes = 48*60 - elapsed_minutes;
-		
-    	// Четное чтение файла
-        // Вычисляем количество минут, прошедших с 13:00 прошлого дня
-        // и выводим это значение
-        // ...
-
-        // Нечетное чтение файла
-        // Вычисляем количество минут, оставшихся до 13:00 следующего дня
-        // и выводим это значение
-        // ...
-    	if (file_read_count % 2 == 0) {
-    	    pr_info("%d minutes since 13:00 yesterday\n", elapsed_minutes);
-    	} else {
-    	    pr_info("%d minutes until 13:00 tomorrow\n", remaining_minutes);
-    	}
-    
-    	file_read_count++;
-
-    	filp_close(file, NULL);
-    	
-    	msleep(20000); // Приостановка выполнения на 2000 миллисекунд (2 секунды)
+	int elapsed_minutes = minutes_since_midnight;
+    int remaining_minutes = 60 - elapsed_minutes;
+	
+    // Четное чтение файла
+    // Вычисляем количество минут, прошедших с начала текущего часа
+    // и выводим это значение
+    // ...
+    // Нечетное чтение файла
+    // Вычисляем количество минут, оставшихся до следующего часа
+    // и выводим это значение
+    // ...
+    if (read_count % 2 == 0) {
+        pr_info("%d of minutes since the beginning of the current hour\n", elapsed_minutes);
+    } else {
+        pr_info("%d of minutes remaining until the next hour\n", remaining_minutes);
     }
-    
+
+    read_count++;
     return 0;
 }
 
-static void __exit my_exit(void) {
-    pr_info("Module unloaded\n");
+static const struct proc_ops proc_ops = {
+    .proc_read = proc_read,
+};
+
+static int proc_init(void) {//создаем файл для последующего чтения
+    proc_create(PROC_NAME, 0, NULL, &proc_ops);
+    pr_info( "/proc/%s created\n", PROC_NAME);
+    return 0;
 }
 
-module_init(my_init);
-module_exit(my_exit);
+static void proc_exit(void) {//Удаляем файл из системы procfs
+    remove_proc_entry(PROC_NAME, NULL);
+    pr_info( "/proc/%s removed\n", PROC_NAME);
+}
+
+module_init(proc_init);
+module_exit(proc_exit);
